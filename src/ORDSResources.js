4b2476fa18b8354cc625d2ffe315138223e500c1
Alejandro Leyva <jleyva868@gmail.com>
2021-11-29 04:29:31 -0600

    first commit

---
+++ src/ORDSResources.js
@@ -0,0 +1,65 @@
+// /src/ORDSResources.js
+import fetch from "node-fetch";
+//import {data} from './index.js';
+
+export default class ORDSResource {
+    constructor( pUrl ){
+      this.url = pUrl;
+    }
+
+    async loadLinks(){
+        return await fetch( this.url, {
+          method: 'GET'
+        } )
+          .then( pResponse => pResponse.json())
+          .then(data => data.links);
+      }
+
+    async loadAll(){
+      return await fetch( this.url, {
+        method: 'GET'
+      } )
+        .then( pResponse => pResponse.json())
+        .then(data => data.items);
+    }
+
+    async loadOne(id){
+        return await fetch( this.url + '/' + id, {
+            method: 'GET'
+          } )
+            .then( pResponse => pResponse.json())
+            .then(data => data.items);
+    }
+
+    async delete(id){
+        fetch(this.url + '/' + id, {
+            method: 'DELETE',
+            })
+        .then(pResponse => pResponse.text())
+        .then(pResponse => console.log(pResponse))
+    }
+
+    async postData(data){
+        console.log(data);
+        return await fetch(this.url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+              }
+        })
+            .then(function(pResponse) {
+                if(pResponse.ok) {
+                    return pResponse.text()
+                } else {
+                    throw "Error in posting";
+                }
+            })
+            .then(function(texto) {
+                console.log(texto);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// /src/index.js
+import Resource from './ORDSResources.js';
+
+let lResource = new Resource( 'https://gd6c1a14fa23298-db202108261312.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/api/task'); //Put the link of your ORDS api
+const data = {
+    name: 'Nombre',
+    done: 'Y'
+};
+await lResource.postData(data);
+console.log(await lResource.loadAll());
+console.log(await lResource.loadOne(277));
